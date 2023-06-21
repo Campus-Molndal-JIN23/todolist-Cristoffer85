4e64c927c37cec26dde71d9b1943635e33d54d7e
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2023-06-21 14:31:07 +0200

    Test first working G-part (I hope, atleast) The id, text, done, assignedTo attributes are in the file thats being created to mongoDB, atleast.

--- TDDproject/src/test/java/org/campusmolndal/MenuTest.java
+++ TDDproject/src/test/java/org/campusmolndal/MenuTest.java
@@ -1,7 +1,42 @@
 package org.campusmolndal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
 
 class MenuTest {
 
+    @Mock
+    private static MongoDBFacade mongoDBFacade;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        Menu.mongoDBFacade = mongoDBFacade;
+    }
+
+    @AfterEach
+    void tearDown() {
+        reset(mongoDBFacade);
+    }
+
+    @Test
+    void testShowMenu() {
+        String input = "1\n1\nTask 1\nfalse\nJohn Doe\n5\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Menu.showMenu();
+
+        verify(mongoDBFacade).create(1, "Task 1", false, "John Doe");
+        verify(mongoDBFacade).close();
+    }
 }
