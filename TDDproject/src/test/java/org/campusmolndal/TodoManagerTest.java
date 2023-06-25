a054a9c301a705e9191dcd4a8a6b5849dcd69cb5
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2023-06-25 10:49:01 +0200

    Fixed working tests for all the public methods now, finally.

--- TDDproject/src/test/java/org/campusmolndal/TodoManagerTest.java
+++ TDDproject/src/test/java/org/campusmolndal/TodoManagerTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -63,5 +66,153 @@ class TodoManagerTest {
             Document userUpdate = new Document("$push", new Document("todos", todoId));
             verify(userCollection, times(1)).updateOne(eq(userFilter), eq(userUpdate));
         }
+    }
+
+    @Test
+    void readOneTodoTest() {
+        // Prepare test data
+        int todoId = 1;
+        Document todoDocument = new Document("_id", todoId)
+                .append("text", "Sample todo")
+                .append("done", false);
+
+        // Configure the input scanner with the desired input
+        InputStream sysInBackup = System.in; // Backup System.in to restore it later
+        ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
+        System.setIn(in);
+
+        // Mock the MongoDB behavior
+        when(todoCollection.find(any(Document.class))).thenReturn(findIterable);
+        when(findIterable.first()).thenReturn(todoDocument);
+
+        // Call the method under test
+        todoManager.readOneTodo(new Scanner(System.in));
+
+        // Verify the output
+        String expectedOutput = "-------------------" + System.lineSeparator();
+        expectedOutput += "Enter the Todo ID: ";
+        expectedOutput += "------------------------------" + System.lineSeparator();
+        expectedOutput += "Todo ID: 1" + System.lineSeparator();
+        expectedOutput += "Text: Sample todo" + System.lineSeparator();
+        expectedOutput += "Done: false" + System.lineSeparator();
+        expectedOutput += "Assigned To: No assigned users" + System.lineSeparator();
+
+        assertEquals(expectedOutput, outputStream.toString());
+
+        // Restore System.in
+        System.setIn(sysInBackup);
+    }
+
+    @Test
+    void readAllTodosTest() {
+        // Prepare test data
+        List<Document> todoDocuments = Arrays.asList(
+                new Document("_id", 1)
+                        .append("text", "Sample todo 1")
+                        .append("done", false),
+                new Document("_id", 2)
+                        .append("text", "Sample todo 2")
+                        .append("done", true)
+        );
+
+        // Mock the MongoDB behavior
+        when(todoCollection.find()).thenReturn(findIterable);
+        when(findIterable.into(any())).thenReturn(todoDocuments);
+
+        // Call the method under test
+        todoManager.readAllTodos();
+
+        // Verify the output
+        String expectedOutput = "------------------------------" + System.lineSeparator();
+        expectedOutput += "All Todos:" + System.lineSeparator();
+        expectedOutput += "------------------------------" + System.lineSeparator();
+        expectedOutput += "Todo ID: 1" + System.lineSeparator();
+        expectedOutput += "Text: Sample todo 1" + System.lineSeparator();
+        expectedOutput += "Done: false" + System.lineSeparator();
+        expectedOutput += "Assigned To: No assigned users" + System.lineSeparator();
+        expectedOutput += System.lineSeparator();
+        expectedOutput += "------------------------------" + System.lineSeparator();
+        expectedOutput += "Todo ID: 2" + System.lineSeparator();
+        expectedOutput += "Text: Sample todo 2" + System.lineSeparator();
+        expectedOutput += "Done: true" + System.lineSeparator();
+        expectedOutput += "Assigned To: No assigned users" + System.lineSeparator();
+        expectedOutput += System.lineSeparator();
+
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    void updateTodoTest() {
+        // Prepare test data
+        int todoId = 1;
+        Document todoDocument = new Document("_id", todoId)
+                .append("text", "Sample todo")
+                .append("done", false);
+
+        // Configure the input scanner with the desired input
+        InputStream sysInBackup = System.in; // Backup System.in to restore it later
+        ByteArrayInputStream in = new ByteArrayInputStream((
+                todoId + System.lineSeparator() +
+                        "Updated text" + System.lineSeparator() +
+                        "true" + System.lineSeparator()
+        ).getBytes());
+        System.setIn(in);
+
+        // Mock the MongoDB behavior
+        when(todoCollection.find(any(Document.class))).thenReturn(findIterable);
+        when(findIterable.first()).thenReturn(todoDocument);
+        when(todoCollection.updateOne(any(Document.class), any(Document.class))).thenReturn(null);
+
+        // Call the method under test
+        todoManager.updateTodo(new Scanner(System.in));
+
+        // Verify the output
+        String expectedOutput = "------------------" + System.lineSeparator();
+        expectedOutput += "Enter the ID of the Todo: " +
+                "Enter the updated text of the Todo: " +
+                "Enter the updated status of the Todo (true/false): " +
+                "Todo updated successfully." + System.lineSeparator();
+
+        assertEquals(expectedOutput, outputStream.toString());
+
+        // Verify the update document passed to the todoCollection.updateOne() method
+        Document expectedUpdateDocument = new Document("$set", new Document("text", "Updated text").append("done", true));
+        verify(todoCollection, times(1)).updateOne(eq(new Document("_id", todoId)), eq(expectedUpdateDocument));
+
+        // Restore System.in
+        System.setIn(sysInBackup);
+    }
+
+    @Test
+    void deleteTodoTest() {
+        // Prepare test data
+        int todoId = 1;
+
+        // Configure the input scanner with the desired input
+        InputStream sysInBackup = System.in; // Backup System.in to restore it later
+        ByteArrayInputStream in = new ByteArrayInputStream((todoId + System.lineSeparator()).getBytes());
+        System.setIn(in);
+
+        // Mock the MongoDB behavior
+        when(todoCollection.deleteOne(any(Document.class))).thenReturn(null);
+        when(userCollection.updateMany(any(Document.class), any(Document.class))).thenReturn(null);
+
+        // Call the method under test
+        todoManager.deleteTodo(new Scanner(System.in));
+
+        // Verify the output
+        String expectedOutput = "-------------------" + System.lineSeparator();
+        expectedOutput += "Enter the ID of the Todo: " +
+                "Todo deleted successfully." + System.lineSeparator();
+
+        assertEquals(expectedOutput, outputStream.toString());
+
+        // Verify the deleteOne and updateMany methods were called with the correct filters
+        verify(todoCollection, times(1)).deleteOne(eq(new Document("_id", todoId)));
+        verify(userCollection, times(1)).updateMany(eq(new Document("todos", todoId)),
+                eq(new Document("$pull", new Document("todos", todoId))));
+
+        // Restore System.in
+        System.setIn(sysInBackup);
     }
 }
--- TDDproject/src/test/java/org/campusmolndal/UserManagerTest.java
+++ TDDproject/src/test/java/org/campusmolndal/UserManagerTest.java
@@ -8,10 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -25,11 +30,14 @@ class UserManagerTest {
     private FindIterable<Document> findIterable;
 
     private UserManager userManager;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalSystemOut = System.out;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
         userManager = new UserManager(userCollection, todoCollection);
+        System.setOut(new PrintStream(outputStream));
     }
 
     @Test
@@ -51,5 +59,97 @@ class UserManagerTest {
         userManager.createUser(scanner);
 
         verify(userCollection, times(1)).insertOne(eq(userDocument));
+    }
+
+    @Test
+    void readUserTest() {
+        // Prepare input stream with user ID
+        String input = "1" + System.lineSeparator();
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        // Prepare expected output
+        String lineSeparator = System.lineSeparator();
+        String expectedOutput = "-------------------" + lineSeparator +
+                "Enter the ID of the User: " +
+                "User ID: 1" + lineSeparator +
+                "Name: John" + lineSeparator +
+                "Age: 25" + lineSeparator +
+                "##############################" + lineSeparator +
+                "Todos:" + lineSeparator +
+                "##############################" + lineSeparator +
+                "No Todos assigned to this user." + lineSeparator;
+
+        // Prepare mocked data
+        Document userDocument = new Document("_id", 1)
+                .append("name", "John")
+                .append("age", 25)
+                .append("todos", new ArrayList<Integer>());
+
+        when(userCollection.find(any(Document.class))).thenReturn(findIterable);
+        when(findIterable.first()).thenReturn(userDocument);
+
+        // Call the method
+        userManager.readUser(new Scanner(System.in));
+
+        // Verify the output
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
+    @Test
+    void updateUserTest() {
+        // Prepare input stream with user ID and updated name
+        String input = "1" + System.lineSeparator() + "John Doe" + System.lineSeparator();
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        // Prepare expected output
+        String lineSeparator = System.lineSeparator();
+        String expectedOutput = "-------------------" + lineSeparator +
+                "Enter the ID of the User: " +
+                "Enter the updated name of the User: " +
+                "User updated successfully." + lineSeparator;
+
+        // Prepare mocked data
+        Document userDocument = new Document("_id", 1)
+                .append("name", "John")
+                .append("age", 25)
+                .append("todos", new ArrayList<Integer>());
+
+        when(userCollection.find(any(Document.class))).thenReturn(findIterable);
+        when(findIterable.first()).thenReturn(userDocument);
+
+        // Call the method
+        userManager.updateUser(new Scanner(System.in));
+
+        // Verify the output
+        assertEquals(expectedOutput, outputStream.toString());
+
+        // Verify the updated document
+        Document expectedUpdateDocument = new Document("$set", new Document("name", "John Doe"));
+        verify(userCollection, times(1)).updateOne(eq(new Document("_id", 1)), eq(expectedUpdateDocument));
+    }
+
+    @Test
+    void deleteUserTest() {
+        // Prepare input stream with user ID
+        String input = "1" + System.lineSeparator();
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        // Prepare expected output
+        String lineSeparator = System.lineSeparator();
+        String expectedOutput = "-------------------" + lineSeparator +
+                "Enter the ID of the User: " +
+                "User deleted successfully." + lineSeparator;
+
+        // Call the method
+        userManager.deleteUser(new Scanner(System.in));
+
+        // Verify the output
+        assertEquals(expectedOutput, outputStream.toString());
+
+        // Verify the delete operation
+        verify(userCollection, times(1)).deleteOne(eq(new Document("_id", 1)));
     }
 }
